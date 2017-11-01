4f7d97c679617407a736e9b76f2f8a4b3612e05e
Miguel <migu3l1991@hotmail.com>
2017-11-01 17:41:17 -0500

    Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { Observable } from 'rxjs/Observable';
+import { browser } from 'protractor';
 import { async } from '@angular/core/testing';
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/pluck';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/repeat';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +18,15 @@ import 'rxjs/add/operator/repeat';
 export class AppComponent {
   title = 'app';
   url = 'https://api.chucknorris.io/jokes/random';
-  urlImage;
+  urlImage = 'https://api.adorable.io/avatars/86/';
   post$;
+  testt;
   constructor(private httpClient: HttpClient) {
+    this.testt = Observable.timer(1, 6000);
     this.getNews();
   }
   getNews() {
-    this.urlImage = 'https://api.adorable.io/avatars/86/' + Math.random();
-    this.post$ = this.httpClient.get(this.url).pluck('value').delay(6000).repeat();
+    this.post$ = this.httpClient.get(this.url).pluck('value').delay(6000).repeat().startWith('Waiting for message...');
+    this.urlImage +=  Math.random();
   }
 }
